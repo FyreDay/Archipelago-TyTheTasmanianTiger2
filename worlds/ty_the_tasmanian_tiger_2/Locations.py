03ec1e159c5e222bdc08b1d4e5a87fd8cf2e39c1
FyreDay <fyredaymc@gmail.com>
2025-05-12 00:24:32 -0500

    Planning

---
+++ worlds/ty_the_tasmanian_tiger_2/Locations.py
@@ -0,0 +1,17 @@
+
+
+locations = {
+    "Platinum Cogs": 0x00,
+    "Chromium Orbs": 0x00,
+    "Bilbies": 0x00,
+    "Disguised Frills": 0x00,  # sanity?
+    "Steve": 0x00, #sanity
+    "Picture Frames": 0x00, #sanity
+    "Mission Complete": 0x00,
+    "rang shop Items": 0x00, # randomize cost
+    "Sly shop Items": 0x00,
+    "Cop shop Items": 0x00, # all cog costs are 3 or randomize based on classification
+    "Madam shop Items": 0x00,
+    "Races": 0x00, #sanity
+    "sign sanity": 0x00 #sanity
+}
